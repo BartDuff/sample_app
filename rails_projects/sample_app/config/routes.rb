3c2a3d665017205ddd414289331d45becbda28ac
Florian Bartkowiak <florian.bartkowiak@gmail.com>
2014-08-10 22:58:45 +0800

    Stopped before logout implementation

--- rails_projects/sample_app/config/routes.rb
+++ rails_projects/sample_app/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
     resources :users
+    resources :sessions, :only => [:new, :create, :destroy]
+    
   
     get '/signup' => 'users#new'
     get '/contact' => 'pages#contact'
     get '/about' => 'pages#about'
     get '/help' => 'pages#help'
+    get '/login' => 'sessions#new'
+    get '/signout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
