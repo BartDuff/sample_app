3d50a44b0bd5b6cb5b466946b32bac1b5c4bd7e6
Florian Bartkowiak <florian.bartkowiak@gmail.com>
2014-08-10 16:19:56 +0800

    End of layout and roots

--- rails_projects/sample_app/config/routes.rb
+++ rails_projects/sample_app/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-    match '/contact', :to => 'pages#contact'
-    match '/about',   :to => 'pages#about'
-    match '/help',    :to => 'pages#help'
+  get 'users/new'
+  
+    get '/signup' => 'users#new'
+    get '/contact' => 'pages#contact'
+    get '/about' => 'pages#about'
+    get '/help' => 'pages#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
