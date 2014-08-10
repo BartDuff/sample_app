31523cfb87b9ec1011716dff9ad2220eecffe16c
Florian Bartkowiak <florian.bartkowiak@gmail.com>
2014-08-10 22:06:23 +0800

    Finished registration

--- rails_projects/sample_app/app/controllers/users_controller.rb
+++ rails_projects/sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
   def new
+      @user = User.new
       @title = "Register"
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    flash[:success] = "Bienvenue dans l'Application Exemple!"
+    redirect_to @user
+    else
+        @title = "Registration"
+        render 'new'
+    end
+    
   end
   
   def show
@@ -11,6 +24,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
